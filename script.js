d72cb470c56f3a843c9c9a416c1a7ef37dcb7fcc
erik schievenin <erik.schievenin@gmail.com>
2022-03-08 17:13:20 +0100

    Exercise complete

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 console.log('JS OK!');
 
+//Per i multipli di 3 stampi “Fizz” al posto del numero e per i 
+//multipli di 5 stampi Buzz.
+
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+const divSquares = document.querySelector("#squares");
+
+let x;
+
+for (let x = 1; x <= 100; x++) {
+
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log('FizzBuzz');
+
+        let element = `<div class="square">FizzBuzz</div>`;
+        divSquares.innerHTML += element;
+    }
+
+    else if (x % 3 === 0) {
+        console.log('Fizz');
+
+        let element = `<div class="square square-blue">Fizz</div>`;
+        divSquares.innerHTML += element;
+    }
+
+    else if (x % 5 === 0) {
+        console.log('Buzz');
+
+        let element = `<div class="square">Buzz</div>`;
+        divSquares.innerHTML += element;
+    }
+
+    else {
+        console.log(x);
+
+        let element = `<div class="square">${x}</div>`;
+        divSquares.innerHTML += element;
+    }
+
+}
+
+const elementSquare = document.querySelectorAll(".square");
+
+elementSquare.style.transitionDelay = '50ms';
+elementSquare.style.transitionProperty = 'background-color';
+elementSquare.style.transitionDuration = '2s';
